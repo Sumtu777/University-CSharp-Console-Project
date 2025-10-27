e65038aa18a8827049689878624436ba054bf21e
Sumtu777 <tanvirul778@outlook.com>
2025-10-27 10:09:03 +0000

    new  code

--- Week 4/Basic Calculator/Program.cs
+++ Week 4/Basic Calculator/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+using System.Data.Common; 
+namespace Hello  
+
+{  
+    class Program  
+    {
+        static void Main(string[] args)
+
+        {
+            // asking user to enter two number 
+
+            Console.Write("Enter the first number: ");
+
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Second number: ");
+
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // asking use to input operator 
+
+            Console.Write("Enter a single Operator (-,+,*,/): ");
+
+            char Operator = Convert.ToChar(Console.ReadLine());
+
+            // checking for valid operator 
+
+            // doing operations 
+
+            if (Operator == '-')
+
+            {
+                Console.WriteLine("Answer is : " + (num1 - num2));
+            }
+
+            else if (Operator == '+')
+
+            {
+                Console.WriteLine("Answer is : " + (num1 + num2));
+            }
+
+            else if (Operator == '*')
+            {
+                Console.WriteLine("Answer is : " + (num1 * num2));
+            }
+
+            else if (Operator == '/')
+            {
+                Console.WriteLine("Answer is : " + (num1 / num2));
+            }
+
+            else
+            {
+                Console.WriteLine("Please, enter a valid operator");
+            }
+        }  
+        
+    }  
+
+} 
---
+++ Week 5/For Loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hello
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int rows = 5; // Number of rows for the pattern
+            for (int i = rows; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+    
+}
